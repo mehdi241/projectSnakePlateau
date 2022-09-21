b12ca564c983ae0d3c98a7ab696bf8f519204eb3
mehdi241 <mehdi.clndc@eduge.ch>
2022-09-21 10:48:24 +0200

    Class Joueur

---
+++ SnakePlateau/Joueur.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Joueur
+{
+    partial class Joueur
+    {
+        Random rnd = new Random();
+        private string _name;
+        private int _step;
+        private int _position;
+        private int _number;
+
+        public Joueur(string name, int step, int position, int number)
+        {
+            _name = name;
+            _step = step;
+            _position = position;
+            _number = number;
+        }
+
+        public Joueur() : this('Gary', 4, 6, 1) { }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Step
+        {
+            get { return _step; }
+            set { _step = value; }
+        }
+
+        public int Position
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+
+        public int Number
+        {
+            get { return _number; }
+            set { _number = value; }
+        }
+        public void Move()
+        {
+            //Random dé 6 faces
+            int dice = rnd.Next(1, 7);
+
+            for (int i = 0; i < 7; i++)
+            {
+                Step = dice;
+                position = position + Step;
+            }
+        }
+    }
+}
