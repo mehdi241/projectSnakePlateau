f46f8f5d1ad89735181be25653966b089695ebd0
LucienFlr <lucien.flr@eduge.ch>
2022-09-28 11:20:37 +0200

    classe question

--- snakePlateau/Question.cs
+++ snakePlateau/Question.cs
@@ -12,6 +12,26 @@ namespace Question
 {
     partial class Question
     {
-        
+        string _question;
+        List<string> _responses;
+        int bonneReponse;
+
+        public Question(string question, List<string> responses)
+        {
+            this._question = question;
+            this._responses = responses;
+        }
+
+        public bool Repondre(int reponse) 
+        {
+            if(reponse == bonneReponse)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
