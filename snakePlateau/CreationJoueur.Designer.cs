d835b7eaf7bfb3e2e24ff17956c7af3e2e5919f6
LucienFlr <lucien.flr@eduge.ch>
2022-10-12 10:41:25 +0200

    creation joeueur ameloriation

--- snakePlateau/CreationJoueur.Designer.cs
+++ snakePlateau/CreationJoueur.Designer.cs
@@ -31,51 +31,68 @@
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.btnStart = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(63, 44);
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(63, 47);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.Size = new System.Drawing.Size(68, 17);
             this.radioButton1.TabIndex = 0;
             this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.Tag = "2";
+            this.radioButton1.Text = "2 joueurs";
             this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             // 
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
             this.radioButton2.Location = new System.Drawing.Point(63, 120);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.Size = new System.Drawing.Size(68, 17);
             this.radioButton2.TabIndex = 1;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.Tag = "3";
+            this.radioButton2.Text = "3 joueurs";
             this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             // 
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
             this.radioButton3.Location = new System.Drawing.Point(63, 198);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.Size = new System.Drawing.Size(68, 17);
             this.radioButton3.TabIndex = 2;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.Tag = "4";
+            this.radioButton3.Text = "4 joueurs";
             this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnStart.Location = new System.Drawing.Point(380, 394);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(110, 48);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Commencer";
+            this.btnStart.UseVisualStyleBackColor = true;
             // 
             // CreationJoueur
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(829, 467);
+            this.ClientSize = new System.Drawing.Size(534, 467);
+            this.Controls.Add(this.btnStart);
             this.Controls.Add(this.radioButton3);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
             this.Name = "CreationJoueur";
-            this.Text = "Création Joueurs";
+            this.Text = "   ";
             this.Load += new System.EventHandler(this.CreationJoueur_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -87,5 +104,6 @@
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button btnStart;
     }
 }
--- snakePlateau/CreationJoueur.cs
+++ snakePlateau/CreationJoueur.cs
@@ -20,34 +20,25 @@ namespace snakePlateau
 
         private void CreationJoueur_Load(object sender, EventArgs e)
         {
-            // Non fonctionnel
-            // Initialize component for 8 gamers
-            for (int i = 0; i < nbJoueurs; i++)
-            {
-                // Create a new label
-                Label lbl = new Label();
-                lbl.Text = "Joueur " + (i + 1);
-                lbl.Location = new Point(10, 10 + (i * 30));
-                lbl.Size = new Size(100, 20);
-                lbl.Visible = true;
-                this.Controls.Add(lbl);
-
-                // Create a new textbox
-                TextBox txt = new TextBox();
-                txt.Location = new Point(120, 10 + (i * 30));
-                txt.Size = new Size(100, 20);
-                txt.Visible = true;
-                this.Controls.Add(txt);
-
-                // Create DomainUpDown
-                DomainUpDown dud = new DomainUpDown();
-                dud.Location = new Point(230, 10 + (i * 30));
-            }
+            
         }
 
         private void nudJoueursNombre_ValueChanged(object sender, EventArgs e)
         {
             //nbJoueurs = nudJoueursNombre.Value; // Non fonctionnel
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton a = sender as RadioButton;
+            for(int i = 0; i< Convert.ToInt32(a.Tag); i++)
+            {
+                composant_joueur composant_Joueur = new composant_joueur();
+                /*composant_Joueur.Width = 30;
+                composant_Joueur.Height = 30;
+                composant_Joueur.Location = new Point(10,10);*/
+                this.Controls.Add(composant_Joueur);
+            }   
         }
     }
 }
